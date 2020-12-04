b1fce16c8a62537906c3ac529faca9addad21c51
egormiron <egormiron@gmail.com>
2020-12-04 17:30:52 +0100

    App.jsx Head.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div className="container-fluid bg-primary">
+    <h3 className="p-2 text-white text-center">Space Articles</h3>
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
-const MainComponent = React.createElement('h1', null, 'Hello, World')
 const container = document.getElementById('root')
 
-ReactDOM.render(MainComponent, container)
+ReactDOM.render(<App />, container)
